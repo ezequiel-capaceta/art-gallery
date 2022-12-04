383f7e96210a2f3bcbc2920a2131df52b9a03208
ezequiel-capaceta <108444379+ezequiel-capaceta@users.noreply.github.com>
2022-12-04 13:48:56 -0800

    User ID Input

--- src/App.js
+++ src/App.js
@@ -7,25 +7,38 @@ import ButtonBar from './Components/ButtonBar.jsx'
 function App() {
   let [artId, setArtId] = useState(190735)
   let [data, setData] = useState({})
+  let [userInput, setUserInput] = useState("")
 
   useEffect(() => {
     
     console.log("useEffect runs")
     document.title = "Welcome to ArtWorld"
 
-    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
-    .then(response => response.json())
-    .then(resData => setData(resData))
+    getArtData(artId)
 
   }, [artId])
+
+  const getArtData = (id) => {
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`)
+    .then(response => response.json())
+    .then(resData => setData(resData))
+  }
 
   const handleIterate = e => {
     setArtId(artId + Number(e.target.value))
   }
 
+  const handleChange = e => {
+    setUserInput(e.target.value)
+  }
+
   return (
     <div className="App">
       <Gallery data={data}/>
+      <div className="user-input">
+        <input type="text" onChange={handleChange} placeholder="Enter art id"/>
+        <button onClick={() => getArtData(userInput)}>Submit</button>
+      </div>
       <ButtonBar handleIterate={handleIterate}/>
     </div>
   );
